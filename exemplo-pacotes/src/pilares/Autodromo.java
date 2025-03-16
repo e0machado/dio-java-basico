ec88e381d10ae467bcbf0d38961c4e7e7459c614
Evandro Machado <evandromachado@outlook.com>
2025-03-16 00:06:17 -0300

    Atualizando GitHub

---
+++ exemplo-pacotes/src/pilares/Autodromo.java
@@ -0,0 +1,19 @@
+package pilares;
+
+public class Autodromo {
+	public static void main(String[] args) {
+		Carro jeep = new Carro();
+		jeep.setChassi("897897");
+		//jeep.ligar();
+		
+		Moto z400 = new Moto();
+		z400.setChassi("467754");
+		//z400.ligar();
+		
+		Veiculo coringa = z400;
+		
+		coringa.ligar();
+		
+	}
+
+}
---
+++ exemplo-pacotes/src/pilares/Carro.java
@@ -0,0 +1,18 @@
+package pilares;
+
+public class Carro extends Veiculo {
+	public void ligar() {
+		confereCambio();
+		confereCombustivel();
+		System.out.println("CARRO LIGADO");
+		
+	}
+	private void confereCombustivel() {
+		System.out.println("conferindo combustivel");
+		
+	}
+	private void confereCambio() {
+		System.out.println("conferindo cambio em P");
+	}
+
+}
